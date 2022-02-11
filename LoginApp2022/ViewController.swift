fa17fd68a4a9d1ae2b42c311ecc85a59e4b6df7b
Roman <roman.d.kozlov@gmail.com>
2022-02-11 20:25:05 +0300

    Initial Commit

---
+++ LoginApp2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp2022
+//
+//  Created by Roman Kozlov on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
