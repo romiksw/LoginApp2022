ab190b6f597c71d3090e8e25a104267331ac222a
Roman <roman.d.kozlov@gmail.com>
2022-02-11 23:50:21 +0300

    login check is done, TF correction is done, TF secure is done

--- LoginApp2022/LoginViewController.swift
+++ LoginApp2022/LoginViewController.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userNameTF.text
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
     }
-    */
+    
+    @IBAction func loginAction() {
+        guard userNameTF.text == "User" else {
+            showAlert(with: "Invalid login or password", and: "Please enter correct")
+            return
+        }
 
+        guard passwordTF.text == "Password" else {
+            showAlert(with: "Invalid login or password", and: "Please enter correct")
+            return
+        }
+        
+        performSegue(withIdentifier: "showWelcomeIdentifier", sender: nil)
+    }
+    
+    @IBAction func forgetNameAction() {
+        showAlert(with: "Oooops", and: "Your name is User!")
+    }
+    
+    @IBAction func forgetPasswordAction() {
+        showAlert(with: "Ooops", and: "Your password is Password")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+}
+
+extension LoginViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
--- LoginApp2022/WelcomeViewController.swift
+++ LoginApp2022/WelcomeViewController.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        welcomeLabel.text = "Welcome, \(userName ?? "")!"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
